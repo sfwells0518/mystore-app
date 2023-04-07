d90ab0d26eeef43184063c66c730fe6d8290823c
Sam Wells <sfwells0518@gmail.com>
2023-04-07 13:09:43 -0500

    remove orders controller and views in prep of generating new controllers for cart items and cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :products
-  resources :orders
+  resources :o
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_07_175659) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_07_180319) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cart_items", force: :cascade do |t|
+    t.bigint "cart_id", null: false
+    t.bigint "product_id", null: false
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
+    t.index ["product_id"], name: "index_cart_items_on_product_id"
+  end
 
   create_table "carts", force: :cascade do |t|
     t.string "name"
@@ -30,4 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_07_175659) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "cart_items", "carts"
+  add_foreign_key "cart_items", "products"
 end
